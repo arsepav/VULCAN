0877a2895a5337fc13b62fa5b02e22b2a94da1b0
GoodDamn <grigorydum@gmail.com>
2024-06-14 21:31:51 +0300

    LikesFragment: Title and desc

--- android/app/src/main/java/good/damn/kamchatka/fragments/ui/LikesFragment.kt
+++ android/app/src/main/java/good/damn/kamchatka/fragments/ui/LikesFragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
 import good.damn.kamchatka.Application
 import good.damn.kamchatka.R
 import good.damn.kamchatka.extensions.boundsFrame
+import good.damn.kamchatka.extensions.setImageDrawableId
+import good.damn.kamchatka.extensions.setTextColorId
+import good.damn.kamchatka.extensions.setTextPx
+import good.damn.kamchatka.extensions.size
 import good.damn.kamchatka.fragments.StackFragment
 import good.damn.kamchatka.views.button.ButtonBack
 
@@ -31,7 +37,53 @@ class LikesFragment
         val btnBack = ButtonBack(
             context
         )
+        val textViewTitle = TextView(
+            context
+        )
+        val textViewDesc = TextView(
+            context
+        )
+        val imageViewLike = AppCompatImageView(
+            context
+        )
 
+        // Typeface
+        textViewTitle.typeface = Application.font(
+            R.font.open_sans_bold,
+            context
+        )
+        textViewDesc.typeface = Application.font(
+            R.font.nunito_regular,
+            context
+        )
+
+
+        // Text color
+        textViewTitle.setTextColorId(
+            R.color.titleColor
+        )
+        textViewDesc.setTextColorId(
+            R.color.titleColor
+        )
+
+
+        // Text size
+        textViewTitle.setTextPx(
+            measureUnit * 0.06949f
+        )
+        textViewDesc.setTextPx(
+            measureUnit * 0.03743f
+        )
+
+
+        // Set up drawable
+        imageViewLike.setImageDrawableId(
+            R.drawable.ic_like_pink
+        )
+
+
+
+        // Stroke color
         btnBack.setStrokeColor(
             Application.color(
                 R.color.titleColor
@@ -44,6 +96,10 @@ class LikesFragment
             measureUnit
         )
         val marginStart = ms * 1.35f
+        layout.size(
+            -1,
+            -1
+        )
         btnBack.boundsFrame(
             Gravity.START,
             size = ButtonBack.btnBackSize(
@@ -54,11 +110,35 @@ class LikesFragment
             ),
             left = ms
         )
+        textViewTitle.boundsFrame(
+            Gravity.START,
+            top = measureUnit * 0.1062f,
+            left = marginStart
+        )
+        textViewDesc.boundsFrame(
+            Gravity.START,
+            top = measureUnit * 0.0507f,
+            left = marginStart
+        )
+        imageViewLike.boundsFrame(
+            Gravity.CENTER,
+            width = (measureUnit * 0.3074f).toInt(),
+            height = (measureUnit * 0.2584f).toInt()
+        )
 
 
         // Adding views
         layout.addView(
             btnBack
+        )
+        layout.addView(
+            textViewTitle
+        )
+        layout.addView(
+            textViewDesc
+        )
+        layout.addView(
+            imageViewLike
         )
 
         btnBack.setOnClickListener(
