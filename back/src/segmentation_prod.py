3d9ba7b80fca7bd4ae52cf7836b4affc8b2c6815
Arsenii <arsepav@gmail.com>
2024-06-15 22:37:25 +0300

    Убрал использование cuda

--- back/src/segmentation_prod.py
+++ back/src/segmentation_prod.py
@@ -17,7 +17,7 @@ def segment_with_coordinates(path_to_jp2, output_path):
     current_directory = os.getcwd()
     print("current_directory", current_directory)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
     print("Использую устройство:", device)
 
     unet = UNet().to(device)
